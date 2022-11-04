2298b4d551383a7423ea1b369f96e7c8bbe6baf5
Petean Anamaria <malinapetean@gmail.com>
2022-11-04 18:45:03 +0200

    ControllerMasina-crud done

--- structuri_de_date_generice/structuri_de_date_generice.cpp
+++ structuri_de_date_generice/structuri_de_date_generice.cpp
@@ -4,33 +4,39 @@
 
 int main()
 {
- //  
-	//Lista<Masina>lista;
-	//ControllerMasina masini;
+   
+	Lista<Masina>lista;
+	ControllerMasina masini;
 
-	//Masina m(2005, "Dacia", "Rosu", 5);
-	//Masina m2(2000, "Audi", "Gri", 5);
-	//Masina m3(1990, "BMW", "Gri", 4);
-	//Masina m4(2010, "Aixam", "Alb", 2);
-	//Masina m5(2011, "Ford", "Alb", 5);
-
-	//lista.addEnd(m);
-	//lista.addEnd(m2);
-	//lista.addEnd(m3);
-	//lista.addEnd(m4);
-	//lista.addPoz(m5, 2);
+	Masina m(1,2005, "Dacia", "Rosu", 5);
+	Masina m2(2,2000, "Audi", "Gri", 5);
+	Masina m3(3,1990, "BMW", "Gri", 4);
+	Masina m4(4,2010, "Aixam", "Alb", 2);
+	Masina m5(5,2011, "Ford", "Alb", 5);
+	Masina m6(2,2003, "Audi", "Galben", 2);
+	/*lista.addEnd(m);
+	lista.addEnd(m2);
+	lista.addEnd(m3);
+	lista.addEnd(m4);
+	lista.addPoz(m5, 2);*/
 	//lista.set(m,1);	
-	////lista.removePoz(1);
-	//Nod<Masina>* aux = lista.iterator();
-	//
-	//while (aux != NULL) {
+	//lista.removePoz(1);
+	Nod<Masina>* aux = lista.iterator();
+	/*
+	while (aux != NULL) {
 
-	//	cout<<aux->data.afisare();
-	//	cout << endl;
-	//	aux = aux->next;
-	//}
+		cout<<aux->data.afisare();
+		cout << endl;
+		aux = aux->next;
+	}*/
 	//cout<<lista.get(2)->data.afisare();
-	//masini.desplay(lista);
+	
+	masini.desplay();
+	/*cout << masini.existenta(m6);
+	cout << masini.addMasina(m6);*/
+	cout << masini.getPosition(m6);
+	masini.updateMasina(m6);
+	masini.desplay();
 }
 
 
